6598ab8dcf2448b43018660e0644974c55c55a15
jeisson-clickup <116575075+jeisson-clickup@users.noreply.github.com>
2024-02-14 09:29:21 -0500

    feat(shard): adding capability to add a shard identifier [CU-8x8uvt7f5] (#24)
    * feat(shard): adding capability to add a shard indentifier [CU-8x8uvt7f5]
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- test/Shard.test.ts
+++ test/Shard.test.ts
@@ -3,28 +3,58 @@ import { IShard, Shard, IShardProps } from '../src';
 const env = 'QA';
 class TestShardImpl extends Shard {
   get name(): string {
-    return `${env}_${this.region}_${this.number}`;
+    const suffix = this.identifier ?? this.number;
+    return `${env}_${this.region}_${suffix}`;
   }
 }
 
-const testShardProps: IShardProps = { cidr: 'fakeCidr', region: 'us-west-2', number: 1 };
+interface TestCase {
+  shardsProps: IShardProps;
+  expectedShardName: string;
+  isShardIdentifierUndefined?: boolean;
+}
+const testCases: TestCase[] = [
+  {
+    shardsProps: { cidr: 'fakeCidr', region: 'us-west-2', number: 1 },
+    expectedShardName: 'QA_us-west-2_1',
+    isShardIdentifierUndefined: true,
+  },
+  {
+    shardsProps: { cidr: 'fakeCidr', region: 'us-east-2', number: 1, identifier: 'global' },
+    expectedShardName: 'QA_us-east-2_global',
+    isShardIdentifierUndefined: false,
+  },
+];
 
-let shard: IShard;
-describe('Shard', () => {
-  beforeEach(() => {
-    shard = new TestShardImpl(testShardProps);
-  });
+describe.each<TestCase>(testCases)(
+  'Shard implementation: $expectedShardName',
+  ({ shardsProps, expectedShardName, isShardIdentifierUndefined }) => {
+    let shard: IShard;
 
-  it('sets cidr correctly', () => {
-    expect(shard.cidr).toEqual(testShardProps.cidr);
-  });
-  it('sets region correctly', () => {
-    expect(shard.region).toEqual(testShardProps.region);
-  });
-  it('sets number correctly', () => {
-    expect(shard.number).toEqual(testShardProps.number);
-  });
-  it('sets name correctly', () => {
-    expect(shard.name).toEqual('QA_us-west-2_1');
-  });
-});
+    beforeEach(() => {
+      shard = new TestShardImpl(shardsProps);
+    });
+
+    it('sets cidr correctly', () => {
+      expect(shard.cidr).toEqual(shardsProps.cidr);
+    });
+    it('sets region correctly', () => {
+      expect(shard.region).toEqual(shardsProps.region);
+    });
+    it('sets number correctly', () => {
+      expect(shard.number).toEqual(shardsProps.number);
+    });
+    it('sets name correctly', () => {
+      expect(shard.name).toEqual(expectedShardName);
+    });
+    if (isShardIdentifierUndefined) {
+      it('identifier is undefined', () => {
+        expect(shard.identifier).toBeUndefined();
+      });
+    } else {
+      it('sets identifier correctly', () => {
+        expect(shard.identifier).toEqual(shardsProps.identifier);
+      });
+    }
+  },
+);
